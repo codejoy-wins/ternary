7a9c0717cb297a974b9307d8d4c11034ea3d4eae
Max <mjann@ucsc.edu>
2019-02-10 00:01:09 -0800

    syntax practice

---
+++ sample.js
@@ -0,0 +1,14 @@
+// ternary operators
+// es6
+
+const es6 = (n)=> n>6 ? true: false
+
+console.log(es6(55));
+console.log(es6(4));
+
+function oddWorld(n){
+    return n%2 == 0 ? false: true;
+}
+
+console.log(oddWorld(7));
+console.log(oddWorld(8));
